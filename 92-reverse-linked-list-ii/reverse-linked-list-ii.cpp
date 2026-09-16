f475c3fce7392f3ad435128d537f58d8b247809b
kunalp <kunalpatil90235@gmail.com>
2026-09-16 22:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (38.82%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,41 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+        int pos = 1;
+        ListNode* t = head;
+        ListNode* before = nullptr;
 
-        if (head == nullptr || left == right)
+        while(pos < left){
+            before = t;
+            t = t->next;
+            pos++;
+        }
+
+        ListNode* curr = t;
+        ListNode* prev = nullptr;
+
+        int times = right - left + 1;
+
+        while(times--){
+            ListNode* nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }
+
+        t->next = curr;
+
+        if(before){
+            before->next = prev;
             return head;
-
-        ListNode dummy(0);
-        dummy.next = head;
-
-        ListNode* prev = &dummy;
-
-        // Move prev to the node just before left
-        for (int i = 1; i < left; i++) {
-            prev = prev->next;
         }
 
-        // Start reversing
-        ListNode* curr = prev->next;
-
-        for (int i = 0; i < right - left; i++) {
-            ListNode* temp = curr->next;
-
-            curr->next = temp->next;
-            temp->next = prev->next;
-            prev->next = temp;
-        }
-
-        return dummy.next;
+        return prev;
     }
 };
